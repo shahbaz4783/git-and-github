b67347f3894c4e3f848ee65d61dad9bc01026cf5
shahbaz4783 <shahbaz4783@gmail.com>
2023-10-22 00:29:20 +0530

    name of branch in js

--- codebase/index.js
+++ codebase/index.js
@@ -3,3 +3,9 @@ const superheroes = require('superheroes');
 let mySuperHero = superheroes.random();
 
 console.log(mySuperHero);
+
+
+const fName = 'conflict';
+const lName = '1';
+
+const fullname = fName + lName;
